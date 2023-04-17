bdeba3d5f858fa0bf6852fd55ab05dca57ddea3d
hanel617 <hyun366720@naver.com>
2023-04-17 17:43:29 +0900

    test brightness3

---
+++ ch05/test_b2_show.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+
+src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
+
+if src is None:
+    print('error')
+
+dst = np.empty(src.shape, src.dtype)
+print(src.dtype)
+
+for y in range(src.shape[0]):
+    for x in range(src.shape[1]):
+        dst[y,x] = src[y,x] + 100
+        
+        
+cv2.imshow('dst', dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
